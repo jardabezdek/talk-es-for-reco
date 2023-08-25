1bd7e8db6a3b2a05a1144982232f3f75bed86fdd
Jaroslav Bezdek <jaroslav.bezdek@strv.com>
2023-08-25 19:24:38 +0800

    chore(data): change posts fields order

--- data/posts.py
+++ data/posts.py
@@ -1,18 +1,18 @@
 DOCS_POSTS = [
     {
         "id": 1,
-        "user_id": 1,
         "description": "Let's dance together! - part 1",
+        "user_id": 1,
         "booster_followers": [2, 3, 8, 19],
         "booster_trending": 1,
         "penalty_consumed_by_users": [3, 8],
     },
     {
         "id": 2,
-        "user_id": 1,
         "description": "Let's dance together! - part 2",
+        "user_id": 1,
         "booster_followers": [2, 3, 8, 19],
-        "booster_trending": 0,
+        "booster_trending": 1,
         "penalty_consumed_by_users": [3],
     },
 ]
